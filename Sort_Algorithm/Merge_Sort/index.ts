738152ad79eb5dcadc6ee2bb69fe770f5168504a
wangzhx <1132154046@qq.com>
2022-11-13 17:52:02 +0800

    add

--- Sort_Algorithm/Merge_Sort/index.ts
+++ Sort_Algorithm/Merge_Sort/index.ts
@@ -1,9 +1,56 @@
+// /**
+//  *  使用哨兵
+//  */
+// function merge(list: Array<number>, start: number, pivot: number, end: number) {
+//     const lindex = pivot - start + 1
+//     const rindex = end - pivot
+
+//     const l: number[] = []
+//     const r: number[] = []
+
+//     for (let i = 1; i <= lindex; i++) {
+//         l[i] = list[start + i - 1]
+//     }
+
+//     for (let i = 1; i <= rindex; i++) {
+//         r[i] = list[pivot + i]
+//     }
+
+//     l[lindex + 1] = Infinity
+//     r[rindex + 1] = Infinity
+
+//     let i = 1
+//     let j = 1
+//     let k = start
+
+//     while (k <= end) {
+//         l[i] < r[j] ? (list[k] = l[i++]) : (list[k] = r[j++])
+
+//         k += 1
+//     }
+// }
+
+// function mergeSort(list: Array<number>, start: number, end: number) {
+//     if (start < end) {
+//         const pivot = Math.floor((start + end) / 2)
+
+//         mergeSort(list, start, pivot)
+//         mergeSort(list, pivot + 1, end)
+
+//         merge(list, start, pivot, end)
+//     }
+// }
+
+// const list = [2, 4, 5, 7, 1, 2, 3, 6]
+
+// mergeSort(list, 0, list.length - 1)
+
+// console.log('merge sort result ', list)
+
+// export {}
+
 /**
  *  使用哨兵
- * @param list
- * @param start
- * @param pivot
- * @param end
  */
 function merge(list: Array<number>, start: number, pivot: number, end: number) {
     const lindex = pivot - start + 1
@@ -29,9 +76,10 @@ function merge(list: Array<number>, start: number, pivot: number, end: number) {
 
     while (k <= end) {
         l[i] < r[j] ? (list[k] = l[i++]) : (list[k] = r[j++])
-
         k += 1
     }
+
+    console.log(l, '', r)
 }
 
 function mergeSort(list: Array<number>, start: number, end: number) {
